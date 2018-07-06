d3aeb7b203c0eb2e022024a25e876bebc56305a3
Guillermo Mazzola <gmazzo65@gmail.com>
2018-07-06 16:34:50 -0400

    Fixed DownloadWSDL task

--- plugin/src/main/groovy/com/github/gmazzo/gradle/plugins/tasks/DownloadWSDL.groovy
+++ plugin/src/main/groovy/com/github/gmazzo/gradle/plugins/tasks/DownloadWSDL.groovy
@@ -1,5 +1,6 @@
 package com.github.gmazzo.gradle.plugins.tasks
 
+import groovy.xml.XmlUtil
 import org.gradle.api.DefaultTask
 import org.gradle.api.internal.file.FileOperations
 import org.gradle.api.tasks.Input
@@ -7,6 +8,7 @@ import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 
 import javax.inject.Inject
+import java.nio.file.Paths
 
 class DownloadWSDL extends DefaultTask {
     private final FileOperations fileOperations
@@ -57,27 +59,22 @@ class DownloadWSDL extends DefaultTask {
         }
 
         // parses the content and downloads the imports too
-        new XmlSlurper()
-                .parse(targetFile)
-                .'**'.findAll({ it.name() == 'import' })
-                .each {
+        def wsdl = new XmlSlurper(false, false).parse(targetFile)
+        wsdl.'**'.findAll({ it.name().matches('(.*:)?import$') }).each {
             String location = it.@schemaLocation.text()
 
             if (location) {
                 if (into == targetFile.parentFile) {
                     // a WSDL with XSD dependencies detected, move them to a grouping folder
-                    def dir = new File(into, entry)
-                    dir.mkdirs()
-
-                    def oldFile = targetFile
-                    targetFile = new File(dir, targetFile.name)
-                    oldFile.renameTo(targetFile)
+                    targetFile.delete()
+                    targetFile = new File(new File(into, entry), targetFile.name)
+                    targetFile.parentFile.mkdirs()
                 }
 
                 def importUrl = url.toURI().resolve(location).toURL()
 
                 if (downloaded.add(importUrl)) {
-                    def name = url.toURI().relativize(importUrl.toURI()).path
+                    def name = new File(importUrl.path).name
                             .replaceFirst('^\\s*$', 'schema')
                             .replaceFirst('(?<!\\.xsd)$', '$0.xsd')
                     def importFile = new File(targetFile.parentFile, name)
@@ -86,8 +83,19 @@ class DownloadWSDL extends DefaultTask {
                                 name.replaceFirst('(?=\\.xsd$)', "_$i"))
                     }
 
+                    def basePath = Paths.get(targetFile.path)
+                    def importPath = Paths.get(importFile.path)
+                    def relativePath = basePath.relativize(importPath)
+                    it.@schemaLocation = relativePath
+
                     downloadAndProcess importFile, entry, importUrl, downloaded, generated
                 }
+            }
+        }
+
+        if (into != targetFile.parentFile) {
+            targetFile.withPrintWriter { pw ->
+                XmlUtil.serialize(wsdl, pw)
             }
         }
     }
